3cf8a19d70d35a37c7ce14a6ad6a9186bb49ec3b
Your Name <you@example.com>
2023-12-22 22:21:46 +0500

    Booked events route

--- controller/attendee.js
+++ controller/attendee.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Attendee = require('../models/attendeeSchema');
 const authenticateUser = require('../authentication');
+const Event= require('../models/eventSchema');
+const Ticket= require('../models/ticketSchema');
 
 const router = express.Router();
 
@@ -38,5 +40,55 @@ router.put('/profile', authenticateUser, async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+router.get('/bookedEvents', authenticateUser, async (req, res) => {
+  try {
+    const attendee = await Attendee.findOne({ email: req.email });
+
+    if (!attendee) {
+      return res.status(404).json({ message: 'Attendee not found' });
+    }
+
+    const bookedEventsSummary = [];
+
+    for(let i=0; i<attendee.bookedEvents.length;i++){
+      const event = await  Event.findById(attendee.bookedEvents[i].eventId.toString());
+      const ticket = await Ticket.findById(attendee.bookedEvents[i].ticketId.toString());
+
+      if (!event||!ticket) {
+        continue; 
+      }
+    
+      const eventId= event._id;
+      const eventName= event.eventName;
+
+      const existingEntry = bookedEventsSummary.find(entry => entry.eventId.toString() === event._id.toString());
+      if (!existingEntry) {
+        // If entry doesn't exist, create a new entry
+        bookedEventsSummary.push({
+          eventId,
+          eventName,
+          totalTickets: 1,
+          regularTickets: ticket.type === 'regular' ? 1 : 0,
+          vipTickets: ticket.type === 'vip' ? 1 : 0,
+        });
+      } else {
+        existingEntry.totalTickets += 1;
+        if (ticket.type === 'regular') {
+          existingEntry.regularTickets += 1;
+        } else if (ticket.type === 'vip') {
+          existingEntry.vipTickets += 1;
+        }
+      }
+      
+
+    }
+
+    res.json(bookedEventsSummary);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 module.exports = router;
